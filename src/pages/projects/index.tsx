f36d4fd270f298b7c3d2a1aa2c14fcd10dd288e1
JonathanMuroma <25608860+JonathanMuroma@users.noreply.github.com>
2024-05-28 19:12:14 +0300

    Create projects pages

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { Person } from "@/utils/types";
+import { Heading, Box } from "@chakra-ui/react";
+import useCompleteQuery from "@/queries/react-query/complete";
+import Projects from "@/components/projects";
+
+export default function ProjectsPage() {
+  return (
+    <>
+      <Projects />
+    </>
+  );
+}
